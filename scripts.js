1c3ab73171e20b8ca6b8cbd62673a72b6bd515e5
Eduardo Soto <A308066@santandertecnologia.com.ar>
2023-05-30 15:09:41 -0300

    Primera preEntrega

---
+++ scripts.js
@@ -0,0 +1,27 @@
+let stock
+
+do{
+    stock = Number(prompt("Ingrese cantidad de productos en stock"))
+}while(isNaN(stock))
+
+do{
+    op=Number(prompt("Elija una opcion del menu\n\n1 - Consultar stock producto\n2 - Vender producto\n0 - Salir"))
+    
+    while(isNaN(op) || (op!==1 && op!==2  && op!==0) ){
+        op=Number(prompt("Debe eligir una opcion correcta. Intente nuevamente."))
+    }
+
+    if (op===1){
+        alert("Hay " + stock + " productos en stock")
+    }else if (op===2){
+        stock--
+    }   
+
+}
+while(op && stock !== 0)
+
+if (!stock){
+    alert("Sin stock")
+}else{
+    alert("Quedan " + stock + " productos en stock")
+}
